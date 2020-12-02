fd7dc384122caa77f5465a0696852ce8f160f575
Osama Khan <Osama6756@gmail.com>
2020-12-02 09:44:26 +0500

    Simplified programs

--- Labs/Lab 6 - Ch 5/Ex1.py
+++ Labs/Lab 6 - Ch 5/Ex1.py
@@ -1,6 +1,9 @@
 # Write a program that counts how many of the
 # squares of the numbers from 1 to 100 end in a 1.
 
-squares = [i*i for i in range(1, 101)]
-nums = [i for i in squares if str(i).endswith("1")]
-print(f"Squares from 1 to 100 ending in 1: {len(nums)}")
+nums = 0
+for i in range(1, 101):
+    power = i*i
+    if str(power).endswith("1"):
+        nums += 1
+print(f"Squares from 1 to 100 ending in 1: {nums}")
--- Labs/Lab 6 - Ch 5/Ex2.py
+++ Labs/Lab 6 - Ch 5/Ex2.py
@@ -1,8 +1,13 @@
 # Write a program that counts how many of the squares of the
 # numbers from 1 to 100 end in a 4 and how many end in a 9.
 
-squares = [i*i for i in range(1, 101)]
-numsEndingIn4 = [i for i in squares if str(i).endswith("4")]
-numsEndingIn9 = [i for i in squares if str(i).endswith("9")]
-print(f"Squares from 1 to 100 ending in 4: {len(numsEndingIn4)}")
-print(f"Squares from 1 to 100 ending in 9: {len(numsEndingIn9)}")
+nums4 = 0
+nums9 = 0
+for i in range(1, 101):
+    power = i*i
+    if str(power).endswith("4"):
+        nums4 += 1
+    if str(power).endswith("9"):
+        nums9 += 1
+print(f"Squares from 1 to 100 ending in 4: {nums4}")
+print(f"Squares from 1 to 100 ending in 9: {nums9}")
